92b0608f478b4911e305d9ac5278c810ae4b2589
daryaobukhova1706 <darya-obuhova-2003@mail.ru>
2022-02-19 20:49:42 +0300

    создание команды help через command interface
    Попытка запустить вывод команды хелп через реализацию интерфейса и pArAmEtErS завершилась неудачей :/

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import Commands.Command;
+import Commands.HelpCommand;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main (String [] args){
-
+        Map<String, Command> commands = new HashMap<>();
+        commands.put("help", new HelpCommand());
 
     }
 }
